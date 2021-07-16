e74aa37c0d26d411cc24e05db65d26ad82327e5e
JustSlash <olegbaraban46@gmail.com>
2021-07-16 20:06:35 +0300

    test in main branch

---
+++ first_steps_in_js/loop.js
@@ -0,0 +1,26 @@
+// let student = {
+//     name: "John",
+//     age: 25,
+//     degree: "Bachelor"
+// }
+
+// for(let item in student) {
+//     console.log(item);
+// }
+
+
+let sum = 0;
+
+for (let i = 0; i <= 10; i++) {
+    kek = sum + i;
+
+    //sum += i;
+
+    if (i == 10) {
+        break;
+    }
+
+    console.log(kek);
+    //console.log(sum);
+
+}
