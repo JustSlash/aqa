96cb9c680c9216ac87856dbcb02085eeaa03cda0
JustSlash <olegbaraban46@gmail.com>
2021-07-16 19:50:13 +0300

    test

---
+++ first_steps_in_js/arr.js
@@ -0,0 +1,48 @@
+let arr1 = Array();
+let arr2 = [1];
+let arr3 = [12345, 'string', 'string1', 67890, 2222, 'string3', {name: "John"}, true, undefined, function() {console.log('hello from array')}];
+let arr4 = arr3;
+
+
+let fruits = ['apple', 'orange'];
+
+let arr5 = fruits.concat(arr3);
+
+let arr6 = [1, 2, 'String', true, undefined, 1222.3, 'Never', 8, 4444, {name: "Hehe"}, 'lul'];
+
+console.log(arr6.indexOf(9));
+
+
+// arr2.push('stringInArr');
+
+// console.log(arr2);
+
+// arr2.pop();
+// console.log(arr2);
+
+// arr2.unshift('kek');
+// console.log(arr2);
+
+// arr2.shift();
+// console.log(arr2);
+
+// for (let some_str of arr5){
+//     console.log(some_str);
+// }
+
+// let bigArr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+
+// for(let i = 0; i < bigArr.length; i++){
+
+//     let smallArr = bigArr[i];
+
+//     for(let k = 0; k < smallArr.length; k++){
+//         console.log(smallArr[k]);
+//     }
+// }
+
+// console.log(String(arr3));
